8d13172279b71e15213eeda1ac484d5db2e96baf
Aslan Ashtiani <saylar.design@gmail.com>
2021-06-19 12:41:26 +0430

    correct css type module

--- components/homepage/card.tsx
+++ components/homepage/card.tsx
@@ -23,7 +23,7 @@ const Cards = () => {
         <Card.Body>
           <Card.Title>We do home loans</Card.Title>
           <Card.Text>
-            We'll help you find the right home loan at a great rate. &#39
+            We'll help you find the right home loan at a great rate.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
